d19a04199d713ce101a0f9dba242e39e0c218574
unknown <C14714685@BUS-3079-21.ict.ad.dit.ie>
2017-11-29 13:17:19 +0000

    initial app

---
+++ app/src/main/java/com/example/c14714685/weatherforecast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c14714685.weatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
